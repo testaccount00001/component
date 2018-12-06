9adc055684c00eac3ea5b5f6ae6112aa65ca4b1c
Autosync <ticket1282.1@test.org>
2018-12-06 08:30:30 +0000

    Commit NewBranch1 12-06-2018 00:30:14

---
+++ aura/light1/light1Controller.js
@@ -0,0 +1,7 @@
+({
+	myAction : function(component, event, helper) {
+		// just for test
+		// yet another line
+	}
+    // extra line
+})
---
+++ aura/light1/light1Renderer.js
@@ -0,0 +1,9 @@
+({
+	// Your renderer method overrides go here
+	// updated? yes/no 2
+	// euro €
+	// updated 2nd time
+	// updated 3rd time
+	// extra line1
+	// extra line2
+})
